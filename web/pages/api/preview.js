b1d5a97cf3af13ad91ea1fbee0d7fc9120daa04a
Leonhard Esterbauer <leonhard.esterbauer@gmail.com>
2020-07-22 14:02:45 +0200

    added next app and document

--- web/pages/api/preview.js
+++ web/pages/api/preview.js
@@ -6,16 +6,6 @@ export default async function preview(req, res) {
     return res.status(401).json({ message: 'Invalid token' });
   }
 
-  /*
-  // Fetch the headless CMS to check if the provided `slug` exists
-  const post = await getPreviewPostBySlug(req.query.slug)
-
-  // If the slug doesn't exist prevent preview mode from being enabled
-  if (!post) {
-    return res.status(401).json({ message: 'Invalid slug' })
-  }
-  */
-
   // Enable Preview Mode by setting the cookies
   res.setPreviewData({});
 

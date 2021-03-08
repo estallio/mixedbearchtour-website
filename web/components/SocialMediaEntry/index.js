306c2ee90bff2ee7a198c7f6257a8455c6646cfb
Leonhard Esterbauer <leonhard.esterbauer@gmail.com>
2021-03-08 12:00:41 +0100

    finished social media entry

--- web/components/SocialMediaEntry/index.js
+++ web/components/SocialMediaEntry/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 import ExtendedBlockContent from '../../lib/ExtendedBlockContent';
 
-import styles from './styles.sass';
+import styles from './SocialMediaEntry.module.sass';
 
 const SocialMediaEntry = ({ contact }) => (
   <div className={styles.contentBox}>
+    <div className={styles.socialMediaHeading}>
+      <h4>Social Media</h4>
+    </div>
     <div className={styles.left}>
-      <a href={`${contact.instagram.url}`} target="_blank" rel="noreferrer">
+      <a href={`${contact.instagram.href}`} target="_blank" rel="noreferrer">
         <i className={styles.instagram}></i>
       </a>
-      <a href={`${contact.facebook.url}`} target="_blank" rel="noreferrer">
+      <a href={`${contact.facebook.href}`} target="_blank" rel="noreferrer">
         <i className={styles.facebook}></i>
       </a>
     </div>

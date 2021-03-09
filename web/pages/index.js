eeb847d0e11bdc3d47d16b01ca1cc047f5717058
Leonhard Esterbauer <leonhard.esterbauer@gmail.com>
2021-03-09 17:03:38 +0100

    finished startseite

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,72 +1,74 @@
 import React from 'react';
 
-import { fetchStart } from '../lib/api';
+import moment from 'moment';
+import 'moment/locale/de';
+moment.locale('de');
+
+import { fetchStartseite } from '../lib/api';
 import ExtendedBlockContent from '../lib/ExtendedBlockContent';
 
-import DefaultHeader from '../components/DefaultHeader';
+import Meta from '../components/Meta';
 import Layout from '../components/Layout';
+import ContactContext from '../components/ContactContext';
 
-import styles from './index.sass';
+import styles from './index.module.sass';
 
-export async function getStaticProps({ preview = false }) {
-  return { props: await fetchStart(preview) };
+export async function getStaticProps() {
+  return { props: await fetchStartseite() };
 }
 
-const Start = ({
-  introGeneral,
-  introTextTournaments,
-  firstTournamentDate,
-  lastTournamentDate,
-  tournamentYear,
+const Startseite = ({
+  startseite: { introGeneral, introTextTournaments },
+  tournamentStart,
+  tournamentEnd,
+  seo,
   contact,
-  preview
 }) => (
-  <>
-    <DefaultHeader
-      title="Startseite | Mixed Beach-Tour"
-      description="Die Startseite der offiziellen Mixed Beach-Tour Website"
-      contact={contact}
-    />
-    <Layout preview={preview} contact={contact}>
-      <div className={styles.welcome}>
+  <ContactContext.Provider value={contact}>
+    <Meta seo={seo.seoStartseite} />
+    <Layout>
+      <div className={styles.welcomeWrapper}>
         <span className={styles.welcomeTitle}>Willkommen</span>
         <span className={styles.welcomeText}>
           auf der offiziellen Website der
         </span>
       </div>
-      <div className={styles.mbt}>
-        <div className={styles.mbtRaiffeisenTitle}>
-          <span>RAIFFEISEN</span>
+      <div className={styles.rmbtWrapper}>
+        <div className={styles.rmbtRaiffeisenTitle}>
+          <h2>RAIFFEISEN</h2>
         </div>
-        <div className={styles.mbtTitle}>
-          <span>Mixed Beach-Tour</span>
+        <div className={styles.rmbtTitle}>
+          <h1>Mixed Beach-Tour</h1>
         </div>
       </div>
-      <div className={styles.intro}>
+      <div className={styles.introWrapper}>
         <div className={styles.introLeft}>
-          <div className={styles.timespan}>
+          <div className={styles.tournamentTimespan}>
             <span>
-              {firstTournamentDate} - {lastTournamentDate}
+              {moment(tournamentStart.datum).format('DD.MM')} -{' '}
+              {moment(tournamentEnd.datum).format('DD.MM')}
             </span>
           </div>
-          <div className={styles.year}>{tournamentYear}</div>
+          <div className={styles.tournamenYear}>
+            {moment(tournamentEnd.datum).format('YYYY')}
+          </div>
         </div>
         <div className={styles.introRight}>
           <ExtendedBlockContent blocks={introTextTournaments} />
         </div>
       </div>
-      <div className={styles.organisation}>
-        <div className={styles.left}>
+      <div className={styles.organisationWrapper}>
+        <div className={styles.organisationLeft}>
           <div>
             <ExtendedBlockContent blocks={introGeneral} />
           </div>
         </div>
-        <div className={styles.right}>
-          <img src="/media/images/cup-01.png" />
+        <div className={styles.organisationRight}>
+          <img src="/theme/images/cup-01.png" />
         </div>
       </div>
     </Layout>
-  </>
+  </ContactContext.Provider>
 );
 
-export default Start;
+export default Startseite;

86aa8312e1fbb1ba8c00d3227ca42e6c89b162b5
Leonhard Esterbauer <leonhard.esterbauer@gmail.com>
2021-04-04 14:19:16 +0200

    fixed formatting

--- web/lib/sanity.js
+++ web/lib/sanity.js
@@ -5,7 +5,7 @@ const options = {
   // Find your project ID and dataset in `sanity.json` in your studio project
   dataset: 'mixedbeachtour',
   projectId: 'hi354wp7',
-  useCdn: process.env.NODE_ENV === 'production'
+  useCdn: process.env.NODE_ENV === 'production',
   // useCdn == true gives fast, cheap responses using a globally distributed cache.
   // Set this to false if your application require the freshest possible
   // data always (potentially slightly slower and a bit more expensive).
